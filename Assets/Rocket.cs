eedb1913faeae0d4ac1ffd9b4cb45c4a9080b671
kurtnovack <kurt@walltowall.com>
2018-03-24 17:54:59 -0400

    adds keyboard input

---
+++ Assets/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
+            print("boost");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            print("go left");
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            print("go right");
+        }
+    }
+}
