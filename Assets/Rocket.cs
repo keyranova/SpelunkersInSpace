12b50aec91863bbbde9803a2f94b6f285c635f4a
kurtnovack <kurt@walltowall.com>
2018-03-24 20:10:39 -0400

    sets up better left/right thrust control

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 800f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput() {
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
-            rigidBody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = mainThrust * Time.deltaTime;
+
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
 
             if (!audioSource.isPlaying) {
                 audioSource.Play();
@@ -29,11 +35,19 @@ public class Rocket : MonoBehaviour {
         } else {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate() {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physices control of rotation
     }
 }
